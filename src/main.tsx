4cddd5645ffebd12e50ffabfa354813615a57ed3
maxnatalia <Nataliamazur1988@gmail.com>
2024-01-10 10:21:30 +0100

    updated - added ThemeSwitcherProvider for theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./core/App.tsx";
+import { ThemeSwitcherProvider } from "./common/ThemeSwitcher/themeContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ThemeSwitcherProvider>
+      <App />
+    </ThemeSwitcherProvider>
   </React.StrictMode>
 );
