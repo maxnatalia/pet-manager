5cd6ebf281ad017eda0bc698829173467c85dda8
maxnatalia <Nataliamazur1988@gmail.com>
2024-01-10 10:18:41 +0100

    added basic styles for Layout

---
+++ src/features/PetManagerApp/Layout/styled.ts
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  max-width: 1400px;
+  margin: 40px auto;
+  padding: 20px;
+  background-color: ${({ theme }) => theme.color.backgroundPrimary};
+  border-radius: 20px;
+  box-shadow: 0px 16px 58px 0px rgba(9, 10, 51, 0.03),
+    0px -2px 50px 0px rgba(9, 10, 51, 0.02);
+  display: grid;
+  grid-template-columns: max-content 1fr 1fr;
+  grid-template-areas:
+    "header header header"
+    "nav main main"
+    "footer footer footer";
+`;
+
+export const Main = styled.main`
+  grid-area: main;
+  min-height: 90vh;
+`;
