1741f2ec3c6589aefe1cd2a3e336764b79758c3d
maxnatalia <Nataliamazur1988@gmail.com>
2024-01-10 10:21:08 +0100

    updated - implemented ThemeProvider with theme

--- src/core/App.tsx
+++ src/core/App.tsx
@@ -1,13 +1,16 @@
-// import { ThemeProvider } from "styled-components";
-import PetManagerApp from "../features/PetManagerApp";
+import { ThemeProvider } from "styled-components";
+import { themeDark, themeLight } from "./theme";
 import { GlobalStyle } from "./GlobalStyle";
+import PetManagerApp from "../features/PetManagerApp";
+import useThemeContext from "../common/ThemeSwitcher/useThemeContext";
 
 const App = () => {
+  const { isDarkTheme } = useThemeContext();
   return (
-    <>
+    <ThemeProvider theme={isDarkTheme ? themeDark : themeLight}>
       <GlobalStyle />
       <PetManagerApp />
-    </>
+    </ThemeProvider>
   );
 };
 
