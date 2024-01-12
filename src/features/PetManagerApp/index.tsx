a5ba2b584ad00c03fc6cae95d58c3319bb9804fd
maxnatalia <Nataliamazur1988@gmail.com>
2024-01-12 10:18:01 +0100

    created basic routes for PetManagerApp

--- src/features/PetManagerApp/index.tsx
+++ src/features/PetManagerApp/index.tsx
@@ -1,5 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./Layout";
+import Home from "./Home";
+import Form from "./Form";
+import Pets from "./Pets";
+
 const PetManagerApp = () => {
-  return <div>PetManagerApp</div>;
+  return (
+    <BrowserRouter basename="/pet-manager">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/form" element={<Form />} />
+          <Route path="/pets" element={<Pets />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default PetManagerApp;
