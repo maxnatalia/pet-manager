7c14008c5854dbb1e5d742289f5c4a0dd2d0e1ce
maxnatalia <Nataliamazur1988@gmail.com>
2024-01-13 09:51:05 +0100

    added Footer component

---
+++ src/features/PetManagerApp/Layout/Footer/index.tsx
@@ -0,0 +1,31 @@
+import { dynamicYear, email, socialMedia } from "./socialMedia";
+import { StyledFooter, ContactBox, Mail, Author } from "./styled";
+
+const Footer = () => {
+  return (
+    <StyledFooter>
+      <ContactBox>
+        <Mail href={`mailto:${email}`}>{email}</Mail>
+        <div>
+          {socialMedia.map(({ name, link, Icon }) => (
+            <a
+              key={name}
+              href={link}
+              title={name}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <Icon />
+            </a>
+          ))}
+        </div>
+      </ContactBox>
+      <Author>
+        Project and design by Natalia Mazur-Żurek
+        <div> &copy; {dynamicYear} All Rights Reserved</div>
+      </Author>
+    </StyledFooter>
+  );
+};
+
+export default Footer;
