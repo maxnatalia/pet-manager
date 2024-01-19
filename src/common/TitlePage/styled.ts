3ab7d4f123f306bf17a20c1afe7c05a297f99f3b
maxnatalia <Nataliamazur1988@gmail.com>
2024-01-19 09:11:06 +0100

    updated - added media queries and improve margins

--- src/common/TitlePage/styled.ts
+++ src/common/TitlePage/styled.ts
@@ -2,18 +2,28 @@ import styled from "styled-components";
 
 export const Title = styled.h2`
   margin: 0;
+  padding: 20px 0 0 20px;
   display: flex;
   align-items: center;
   gap: 10px;
   text-transform: uppercase;
   font-size: 36px;
   font-weight: 400;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+    font-size: 26px;
+  }
 `;
 
 export const Subtitle = styled.h3`
-  margin: 0;
+  display: inline-block;
+  margin: 10px 0 0 20px;
   padding-top: 10px;
   border-top: 1px solid ${({ theme }) => theme.color.textPrimary};
   font-size: 20px;
   font-weight: 400;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+    font-size: 16px;
+  }
 `;
