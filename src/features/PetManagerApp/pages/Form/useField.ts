b72e3c5b0a6960cad92de13cbd8fd630dcca3467
maxnatalia <Nataliamazur1988@gmail.com>
2024-01-20 06:56:36 +0100

    created custom hook for handling fields in forms

---
+++ src/features/PetManagerApp/pages/Form/useField.ts
@@ -0,0 +1,36 @@
+import { useState, ChangeEvent } from "react";
+import { ValidationFunction } from "../../utils";
+
+type UseFieldProps = {
+  validateValue?: ValidationFunction;
+  initialValue?: string;
+};
+
+const useField = ({ validateValue, initialValue = "" }: UseFieldProps = {}) => {
+  const [enteredValue, setEnteredValue] = useState(initialValue);
+  const [isTouched, setIsTouched] = useState(false);
+
+  const valueIsValid = validateValue ? validateValue(enteredValue) : true;
+  const hasError = !valueIsValid && isTouched;
+
+  const valueChangeHandler = (
+    e: ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>
+  ) => {
+    setEnteredValue(e.target.value);
+  };
+
+  const inputBlurHandler = () => {
+    setIsTouched(true);
+  };
+
+  return {
+    value: enteredValue,
+    hasError,
+    valueChangeHandler,
+    inputBlurHandler,
+    isValid: valueIsValid,
+    setIsTouched,
+    setEnteredValue,
+  };
+};
+export default useField;
