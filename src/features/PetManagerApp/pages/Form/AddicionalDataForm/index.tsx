303d16726845c59d3b880c7ab11b246351f2512c
maxnatalia <Nataliamazur1988@gmail.com>
2024-01-23 11:00:20 +0100

    created AdditionalDataForm component

---
+++ src/features/PetManagerApp/pages/Form/AddicionalDataForm/index.tsx
@@ -0,0 +1,79 @@
+import { ChangeEventHandler } from "react";
+import { petCategory } from "../../../utils";
+import { BlockBox, CustomRadio, FieldBox, Input, Label } from "../styled";
+
+type AdditionalDataFormProps = {
+  categoryChangedHandler: ChangeEventHandler<HTMLSelectElement>;
+  category: string;
+  gender: string;
+  genderChangedHandler: ChangeEventHandler<HTMLInputElement>;
+  description: string;
+  descriptionChangedHandler: ChangeEventHandler<HTMLTextAreaElement>;
+};
+
+const AddicionalDataForm = ({
+  categoryChangedHandler,
+  category,
+  gender,
+  genderChangedHandler,
+  description,
+  descriptionChangedHandler,
+}: AdditionalDataFormProps) => {
+  return (
+    <BlockBox $position="additional">
+      <FieldBox>
+        <Label>Pet category:</Label>
+        <Input as={"select"} onChange={categoryChangedHandler} value={category}>
+          {petCategory.map((category, index) => (
+            <option key={`${index} - ${category.value}`} value={category.value}>
+              {category.label}
+            </option>
+          ))}
+        </Input>
+      </FieldBox>
+      <FieldBox $horizontal>
+        <Label $horizontal>
+          <CustomRadio
+            type="radio"
+            value="male"
+            name="gender"
+            checked={gender === "male"}
+            onChange={genderChangedHandler}
+          />
+          <span>MALE</span>
+        </Label>
+        <Label $horizontal>
+          <CustomRadio
+            type="radio"
+            value="female"
+            name="gender"
+            checked={gender === "female"}
+            onChange={genderChangedHandler}
+          />
+          <span>FEMALE</span>
+        </Label>
+        <Label $horizontal>
+          <CustomRadio
+            type="radio"
+            value="unspecified"
+            name="gender"
+            checked={gender === "unspecified"}
+            onChange={genderChangedHandler}
+          />
+          <span>UNSPECIFIED</span>
+        </Label>
+      </FieldBox>
+      <FieldBox>
+        <Label>Description:</Label>
+        <Input
+          as={"textarea"}
+          name="description"
+          value={description}
+          onChange={descriptionChangedHandler}
+        />
+      </FieldBox>
+    </BlockBox>
+  );
+};
+
+export default AddicionalDataForm;
