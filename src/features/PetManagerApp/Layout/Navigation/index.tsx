1d8e224cb9640f31e90555e4b8b962844df8dcc7
maxnatalia <Nataliamazur1988@gmail.com>
2024-01-24 10:55:43 +0100

    updated - added new condition when routes change

--- src/features/PetManagerApp/Layout/Navigation/index.tsx
+++ src/features/PetManagerApp/Layout/Navigation/index.tsx
@@ -1,20 +1,32 @@
+import { useLocation } from "react-router-dom";
 import { navigationList } from "./navigationList";
 import { Label, List, StyledNavLink, StyledNavigation } from "./styled";
 
 const Navigation = () => {
+  const location = useLocation();
+  const shouldDisplayForm = location.pathname === "/form";
+
   return (
-    <StyledNavigation>
-      <List>
-        {navigationList.map(({ name, link, title, Icon }) => (
-          <li key={link}>
-            <StyledNavLink $special={link === "/form"} to={link} title={title}>
-              <Icon $special={link === "/form"} />
-              <Label $special={link === "/form"}>{name}</Label>
-            </StyledNavLink>
-          </li>
-        ))}
-      </List>
-    </StyledNavigation>
+    <>
+      {!shouldDisplayForm && (
+        <StyledNavigation>
+          <List>
+            {navigationList.map(({ name, link, title, Icon }) => (
+              <li key={link}>
+                <StyledNavLink
+                  $special={link === "/form"}
+                  to={link}
+                  title={title}
+                >
+                  <Icon $special={link === "/form"} />
+                  <Label $special={link === "/form"}>{name}</Label>
+                </StyledNavLink>
+              </li>
+            ))}
+          </List>
+        </StyledNavigation>
+      )}
+    </>
   );
 };
 
