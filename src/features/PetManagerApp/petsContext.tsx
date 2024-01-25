8befc163782cb5863fcd7ea400cf768b88bac636
maxnatalia <Nataliamazur1988@gmail.com>
2024-01-25 11:15:29 +0100

    improved types for functions and changed path for Pet type

--- src/features/PetManagerApp/petsContext.tsx
+++ src/features/PetManagerApp/petsContext.tsx
@@ -1,17 +1,6 @@
 import { createContext, useState } from "react";
 import { initialPetsList } from "./initialPetsList";
-import { EventType, PetCategory } from "./utils";
-
-export type Pet = {
-  id: string;
-  petName: string;
-  breed: string;
-  category: PetCategory;
-  gender: string;
-  description: string;
-  dateOfBirth: string;
-  events: EventType[];
-};
+import { EventType, Pet } from "./types";
 
 type PetsContextType = {
   petsList: Pet[];
@@ -20,10 +9,10 @@ type PetsContextType = {
   setEditableId: React.Dispatch<React.SetStateAction<string>>;
   editableEventId: string;
   setEditableEventId: React.Dispatch<React.SetStateAction<string>>;
-  getPetById: (selectedPetId: string | undefined) => Pet | undefined;
-  handleRemovePet: (id: string) => void;
-  handleEditPet: (id: string) => void;
-  handleEditPetEvent: (petId: string, eventId: string) => void;
+  getPetById: (selectedPetId: Pet["id"] | undefined) => Pet | undefined;
+  handleRemovePet: (id: Pet["id"]) => void;
+  handleEditPet: (id: Pet["id"]) => void;
+  handleEditPetEvent: (petId: Pet["id"], eventId: EventType["id"]) => void;
 };
 
 type PetsProviderProps = {
@@ -37,18 +26,18 @@ export const PetsProvider = ({ children }: PetsProviderProps) => {
   const [editableId, setEditableId] = useState("");
   const [editableEventId, setEditableEventId] = useState("");
 
-  const getPetById = (selectedPetId: string | undefined) =>
+  const getPetById = (selectedPetId: Pet["id"] | undefined) =>
     petsList.find(pet => pet.id === selectedPetId);
 
-  const handleRemovePet = (id: string) =>
+  const handleRemovePet = (id: Pet["id"]) =>
     setPetsList(prevList => prevList.filter(pet => pet.id !== id));
 
-  const handleEditPet = (id: string) => {
+  const handleEditPet = (id: Pet["id"]) => {
     petsList.find(editablePet => editablePet.id === id);
     setEditableId(id);
   };
 
-  const handleEditPetEvent = (petId: string, eventId: string) => {
+  const handleEditPetEvent = (petId: Pet["id"], eventId: EventType["id"]) => {
     const editableEvent = petsList
       .find(pet => pet.id === petId)
       ?.events.find(event => event.id === eventId);
