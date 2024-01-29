4ac9dd237e115ff8c1ed908c70dd11dbf9f80214
maxnatalia <Nataliamazur1988@gmail.com>
2024-01-29 10:58:15 +0100

    added basic styles GridContent for Home

---
+++ src/features/PetManagerApp/pages/Home/styled.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const GridContent = styled.div`
+  display: grid;
+  grid-template-areas:
+    "today pets"
+    "overview pets"
+    "events pets";
+`;
