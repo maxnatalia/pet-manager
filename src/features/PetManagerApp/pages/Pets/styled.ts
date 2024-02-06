8d06afb35f89db0a84adffc62b5984ea99fc0ca9
maxnatalia <Nataliamazur1988@gmail.com>
2024-02-06 08:36:13 +0100

    deleted ButtonIcon style - used common style

--- src/features/PetManagerApp/pages/Pets/styled.ts
+++ src/features/PetManagerApp/pages/Pets/styled.ts
@@ -1,9 +1,4 @@
-import styled, { css } from "styled-components";
-
-type ButtonType = {
-  $edit?: boolean;
-  $remove?: boolean;
-};
+import styled from "styled-components";
 
 export const PetsContainer = styled.div`
   padding: 20px;
@@ -120,46 +115,4 @@ export const ButtonsBox = styled.div`
   position: absolute;
   right: 0;
   top: 0;
-`;
-export const ButtonIcon = styled.button<ButtonType>`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 40px;
-  height: 40px;
-  font-size: 18px;
-  border-radius: 50%;
-  border: 2px solid transparent;
-  background-color: ${({ theme }) => theme.color.special};
-  color: ${({ theme }) => theme.color.backgroundSecondary};
-  cursor: pointer;
-  transition: all 0.5s ease-in-out;
-
-  &:hover {
-    background-color: ${({ theme }) => theme.color.backgroundSecondary};
-    color: ${({ theme }) => theme.color.special};
-    border: 2px solid ${({ theme }) => theme.color.special};
-  }
-
-  ${({ $edit }) =>
-    $edit &&
-    css`
-      background-color: ${({ theme }) => theme.color.edit};
-
-      &:hover {
-        color: ${({ theme }) => theme.color.edit};
-        border: 2px solid ${({ theme }) => theme.color.edit};
-      }
-    `}
-
-  ${({ $remove }) =>
-    $remove &&
-    css`
-      background-color: ${({ theme }) => theme.color.remove};
-
-      &:hover {
-        color: ${({ theme }) => theme.color.remove};
-        border: 2px solid ${({ theme }) => theme.color.remove};
-      }
-    `}
 `;
