93d6770fec877d623af8aebdc7a91e222871afd2
maxnatalia <Nataliamazur1988@gmail.com>
2024-02-08 09:36:20 +0100

    updated - implemented Card component for LatestPets

--- src/features/PetManagerApp/pages/Home/LatestPets/index.tsx
+++ src/features/PetManagerApp/pages/Home/LatestPets/index.tsx
@@ -1,37 +1,19 @@
-import { MdMore } from "react-icons/md";
 import { TitleBox } from "../../../../../common/TitleBox";
-import { getPetEmoji } from "../../../utils";
+import { mapPetsForCard } from "../../../utils";
 import usePetsContext from "../../../usePetsContext";
-import { AddBox, PetItem, PetsBox, PetsList } from "./styled";
-import { ButtonLink } from "../../../../../common/ButtonLink";
-import { useNavigate } from "react-router-dom";
+import { PetsBox } from "./styled";
+
+import Card from "../../../../../common/Card";
 
 const LatestPets = () => {
   const { petsList } = usePetsContext();
-  const navigate = useNavigate();
+
+  const mappedPetsList = mapPetsForCard(petsList);
 
   return (
     <PetsBox>
       <TitleBox>Lastest added pets:</TitleBox>
-      <PetsList>
-        {petsList.map(pet => (
-          <PetItem key={pet.id}>
-            <span>{getPetEmoji(pet.category)}</span>
-            <span>{pet.petName}</span>
-            <AddBox>
-              <span>XX.XX.XXXX</span>
-              <span>adding date</span>
-            </AddBox>
-            <ButtonLink
-              title={`Go to ${pet.petName} page`}
-              onClick={() => navigate(`/pet/${pet.id}`)}
-            >
-              <MdMore />
-              Go
-            </ButtonLink>
-          </PetItem>
-        ))}
-      </PetsList>
+      <Card $column data={mappedPetsList} />
     </PetsBox>
   );
 };
