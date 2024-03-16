c7f460f4c03b681ab203e552eb8200654559f480
maxnatalia <Nataliamazur1988@gmail.com>
2024-03-16 07:21:41 +0100

    added basic styles with props gridPosition and type

---
+++ src/common/BlockBox/styled.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export type GridPosition = "latestPets" | "overview";
+
+export const StyledBlockBox = styled.div<{ $gridPosition?: GridPosition }>`
+  grid-area: ${({ $gridPosition }) => $gridPosition};
+  padding: 20px;
+  border: 1px solid black;
+  /* display: flex;
+  flex-direction: column;
+  gap: 20px; */
+`;
+
+export const Title = styled.h2`
+  margin-top: 0;
+  margin-bottom: 20px;
+`;
