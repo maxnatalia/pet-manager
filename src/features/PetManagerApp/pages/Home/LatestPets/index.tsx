01ca3c8db430046925a6982eecad2aa9af3595ac
maxnatalia <Nataliamazur1988@gmail.com>
2024-03-17 07:06:12 +0100

    deleted PetBox and implemented BlockBox

--- src/features/PetManagerApp/pages/Home/LatestPets/index.tsx
+++ src/features/PetManagerApp/pages/Home/LatestPets/index.tsx
@@ -2,26 +2,23 @@ import BlockBox from "../../../../../common/BlockBox";
 import ItemCard from "../../../../../common/ItemCard";
 import { getPetEmoji } from "../../../utils";
 import usePetsContext from "../../../usePetsContext";
-import { PetsBox } from "./styled";
 
 const LatestPets = () => {
   const { petsList } = usePetsContext();
 
   return (
-    <PetsBox>
-      <BlockBox title="🖇️ Lastest added pets:">
-        {petsList.map(item => (
-          <ItemCard
-            key={item.id}
-            avatar={getPetEmoji(item.category)}
-            name={item.petName}
-            subname={item.breed}
-            linkName="See more..."
-            linkAddress={`/pet/${item.id}`}
-          />
-        ))}
-      </BlockBox>
-    </PetsBox>
+    <BlockBox title="🖇️ Lastest added pets:" $gridPosition="latestPets">
+      {petsList.map(item => (
+        <ItemCard
+          key={item.id}
+          avatar={getPetEmoji(item.category)}
+          name={item.petName}
+          subname={item.breed}
+          linkName="See more..."
+          linkAddress={`/pet/${item.id}`}
+        />
+      ))}
+    </BlockBox>
   );
 };
 
