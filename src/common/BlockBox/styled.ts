aa5f68db945e1b9e9e520f782d3f6e8dd908220e
maxnatalia <Nataliamazur1988@gmail.com>
2024-03-19 12:01:17 +0100

    added new props and component BlockContent

--- src/common/BlockBox/styled.ts
+++ src/common/BlockBox/styled.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export type GridPosition =
   | "latestPets"
@@ -6,16 +6,28 @@ export type GridPosition =
   | "today"
   | "eventsInsights";
 
+export type BlockContentFlow = {
+  $direction?: "column";
+};
+
 export const StyledBlockBox = styled.div<{ $gridPosition?: GridPosition }>`
   grid-area: ${({ $gridPosition }) => $gridPosition};
   padding: 20px;
   border: 1px solid black;
-  /* display: flex;
-  flex-direction: column;
-  gap: 20px; */
 `;
 
-export const Title = styled.h2`
-  margin-top: 0;
-  margin-bottom: 20px;
+export const Title = styled.h3`
+  font-size: 30px;
+  margin: 0;
+`;
+
+export const BlockContent = styled.div<BlockContentFlow>`
+  display: flex;
+  justify-content: space-between;
+
+  ${({ $direction }) =>
+    $direction === "column" &&
+    css`
+      flex-direction: column;
+    `}
 `;
