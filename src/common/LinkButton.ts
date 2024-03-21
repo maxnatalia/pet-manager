6f9cde28cdc7b205396e2acd5520d725333aa82f
maxnatalia <Nataliamazur1988@gmail.com>
2024-03-21 11:25:59 +0100

    improved color, justify-content and border

--- src/common/LinkButton.ts
+++ src/common/LinkButton.ts
@@ -4,12 +4,13 @@ import styled, { css } from "styled-components";
 export const LinkButton = styled(Link)<{ $active?: boolean }>`
   padding: 6px 10px;
   display: flex;
+  justify-content: center;
   align-items: center;
   gap: 6px;
   text-decoration: none;
   text-transform: capitalize;
   font-weight: 700;
-  color: ${({ theme }) => theme.color.special};
+  color: ${({ theme }) => theme.color.textPrimary};
   border-radius: 10px 10px 0 0;
   border-bottom: 1px solid ${({ theme }) => theme.color.special};
   background-color: transparent;
@@ -18,6 +19,7 @@ export const LinkButton = styled(Link)<{ $active?: boolean }>`
 
   &:hover {
     background-color: ${({ theme }) => theme.color.special};
+    border-bottom: 1px solid ${({ theme }) => theme.color.textPrimary};
     color: ${({ theme }) => theme.color.backgroundPrimary};
   }
 
